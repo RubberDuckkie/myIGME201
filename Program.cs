1a8931ee7b26596e352ceae9a5bc56d5fbebdc0d
RubberDuckkie <69916215+RubberDuckkie@users.noreply.github.com>
2020-08-22 15:53:10 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogunwale_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is just a number I decied to come up with.
+            object zan = 8;
+            //I wanted to see what dark yellow would look like on the command prompt.
+            Console.ForegroundColor =
+                ConsoleColor.DarkYellow;
+            //Part of the assignment.
+            Console.WriteLine("Did you know? In Final Fantasy XIV, the Monk Class has references to Bhuddism and other Eastern Religions!");
+            //How I felt today.
+            Console.WriteLine("Today feels like : " + zan);
+        }
+    }
+}
