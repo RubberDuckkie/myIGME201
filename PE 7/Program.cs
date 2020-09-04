f770c05e680bbad9a1f441c3b04f4b10db461b1e
RubberDuckkie <69916215+RubberDuckkie@users.noreply.github.com>
2020-09-03 20:30:54 -0400

    Add files via upload

---
+++ PE 7/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numLibs = 0;
+            int cntr = 0;
+            int nChoice = 0;
+
+            StreamReader input;
+
+            // open the template file to count how many Mad Libs it contains
+            input = new StreamReader("c:\\templates\\MadLibsTemplate.txt");
+
+            string line = null;
+            while ((line = input.ReadLine()) != null)
+            {
+                ++numLibs;
+            }
+
+            // close it
+            input.Close();
+
+            // only allocate as many strings as there are Mad Libs
+            string[] madLibs = new string[numLibs];
+
+            // read the Mad Libs into the array of strings
+            input = new StreamReader("c:\\templates\\MadLibsTemplate.txt");
+
+            line = null;
+            while ((line = input.ReadLine()) != null)
+            {
+                // set this array element to the current line of the template file
+                madLibs[cntr] = line;
+
+                // replace the "\\n" tag with the newline escape character
+                madLibs[cntr] = madLibs[cntr].Replace("\\n", "\n");
+
+                ++cntr;
+            }
+
+            input.Close();
+
+            // prompt the user for which Mad Lib they want to play (nChoice)
+            Console.WriteLine("What story do you want to read? Choose a number between 1 and 6");
+            nChoice = Convert.ToInt32(Console.ReadLine());
+
+
+            // split the Mad Lib into separate words
+            string[] words = madLibs[nChoice].Split(' ');
+
+            foreach (string word in words)
+            {
+                if (word == "{") {
+                    Console.WriteLine("Give me your replacement word");
+                    string newWord = Console.ReadLine();
+                }
+                // prompt the user for the replacement
+                // and append the user response to the result string
+                // else append word to the result string
+            }
+        }
+    }
+}
