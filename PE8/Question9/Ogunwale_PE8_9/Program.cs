bbe51c3c2bafe353ad187c0ba21ece5c6e670183
RubberDuckkie <69916215+RubberDuckkie@users.noreply.github.com>
2020-09-05 15:29:27 -0400

    Add files via upload

---
+++ PE8/Question9/Ogunwale_PE8_9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogunwale_PE_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prompts the user for a string
+            Console.WriteLine("Give me a string, and I'll fill it with quotes!: ");
+            string quo = Console.ReadLine();
+            //splits each of the spaces
+            string[] wordSet = quo.Split(' ');
+            //the fpreach loop to print all the words in the array
+            foreach (string word in wordSet)
+            {   //Puts all the words on a new line
+                Console.WriteLine("\"" + word + "\"");
+            }
+        }
+    }
+}
