b404c0539d3df7c8b8f6003d026d357b6daa5f1a
RubberDuckkie <69916215+RubberDuckkie@users.noreply.github.com>
2020-09-05 15:33:37 -0400

    Update Program.cs

--- PE8/Question8/Ogunwale_PE8_ 8/Program.cs
+++ PE8/Question8/Ogunwale_PE8_ 8/Program.cs
@@ -9,11 +9,15 @@ namespace Ogunwale_PE8__8
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //Takes the user input and replaces every no with a yes
             Console.WriteLine("Give me a string, and ill replace any no with a yes: ");
+            //reads the user input
             string userString = Console.ReadLine();
+            //takes everything into the array
             char[] charArray = userString.ToCharArray();
+            //replaces every no in the string with a yes
             string replaceString = userString.Replace("no", "yes");
+            //presents the replaced string.
             Console.WriteLine("Here is your replaced String!");
             Console.WriteLine(replaceString);
 
