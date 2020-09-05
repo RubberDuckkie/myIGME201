1d96357072414c5ed8b06995cb690f7affa5f598
RubberDuckkie <69916215+RubberDuckkie@users.noreply.github.com>
2020-09-05 15:32:04 -0400

    Update Program.cs

--- PE8/Question7/Ogunwale_PE8_7/Program.cs
+++ PE8/Question7/Ogunwale_PE8_7/Program.cs
@@ -10,12 +10,16 @@ namespace Ogunwale_PE8_7
     {
         static void Main(string[] args)
         {
+            //Takes user input from the user
              Console.WriteLine("Give me a string, and ill reverse it!: ");
              string s = Console.ReadLine();
+            //Tells the user thats reversing
             Console.WriteLine("Reverse, Reverse!");
+            //Takes the user input and puts it into an array
              char[] charArray = s.ToCharArray();
+            //reverses the array.
              Array.Reverse(charArray);
-
+            //prints out the result.
             Console.WriteLine(charArray);
               
             
