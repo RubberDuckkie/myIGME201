abf3bdade5c5e1fb6cb2a23eb931f60c6ecc1855
RubberDuckkie <69916215+RubberDuckkie@users.noreply.github.com>
2020-09-05 11:06:52 -0400

    Add files via upload

---
+++ PE8/Question7/Ogunwale_PE8_7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogunwale_PE8_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Give me a string, and ill reverse it!: ");
+             string s = Console.ReadLine();
+            Console.WriteLine("Reverse, Reverse!");
+             char[] charArray = s.ToCharArray();
+             Array.Reverse(charArray);
+
+            Console.WriteLine(charArray);
+              
+            
+
+        }
+    }
+}
