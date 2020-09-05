f97e04848cd4a2758d47e83fdf1a448b44eef4c0
RubberDuckkie <69916215+RubberDuckkie@users.noreply.github.com>
2020-09-05 15:35:05 -0400

    Add files via upload

---
+++ PE8/Question5/Ogunwale_PE8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogunwale_PE8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //set z to double
+            double z = 0.0;
+            //the x increment formula
+            for (double x = -1; x <= 1; x += 0.1) {
+                //the y increment formula
+                for (double y = 1; y <= 4; y += 0.1) {
+                    //the Z formula
+                    z = (3 * Math.Pow(y, 2)) + (2 * x) - 1;
+                    //prints out the results
+                    Console.WriteLine(z);
+                }
+                //new space
+                Console.WriteLine("\n");
+            }
+            
+
+        }
+    }
+}
