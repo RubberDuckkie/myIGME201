8ac954437a57ae0ace5afe74ffd3f47a4d6c1306
RubberDuckkie <69916215+RubberDuckkie@users.noreply.github.com>
2020-09-05 11:09:51 -0400

    Add files via upload

---
+++ PE8/Question8/Ogunwale_PE8_ 8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogunwale_PE8__8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a string, and ill replace any no with a yes: ");
+            string userString = Console.ReadLine();
+            char[] charArray = userString.ToCharArray();
+            string replaceString = userString.Replace("no", "yes");
+            Console.WriteLine("Here is your replaced String!");
+            Console.WriteLine(replaceString);
+
+        }
+    }
+}
