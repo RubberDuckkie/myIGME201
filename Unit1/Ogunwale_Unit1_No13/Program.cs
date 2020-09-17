10fa9180fbb7ba254e94c9e30db6dc2b4d18c8dc
RubberDuckkie <69916215+RubberDuckkie@users.noreply.github.com>
2020-09-16 22:46:05 -0400

    Add files via upload

---
+++ Unit1/Ogunwale_Unit1_No13/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogunwale_Unit1_No13
+{
+    class Program
+    {
+        struct employee
+        {
+            public string sName;
+            public double dSalary;
+        }
+
+        static void Main(string[] args)
+        {
+
+
+
+        }
+
+        static bool GiveRaise(string name, ref double salary)
+        {
+            employee x;
+            if (name == x.sName)
+            {
+                return true;
+                salary = x.dSalary + 19999.99;
+                Console.WriteLine("Congratulations! Your salary has increased by $19,999.99!! your new salary is: $" + salary);
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+    }
+}
