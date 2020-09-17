ac0053d669f8a4fca80b20a379d476a5b3895c5a
RubberDuckkie <69916215+RubberDuckkie@users.noreply.github.com>
2020-09-16 22:44:21 -0400

    Add files via upload

---
+++ Unit1/Ogunwale_Unit1_No3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogunwale_Unit1_No3
+{
+    class Program
+    {
+        //creates a delegate
+        public delegate string ReadingLine();
+        static void Main(string[] args)
+        {
+            //this is the variable of delegate 
+            ReadingLine printing;
+
+            //now, to construct the delegate function
+            printing = new ReadingLine(Console.ReadLine);
+
+            //and finally, to call the delegate function
+            string readString = printing();
+
+        }
+    }
+}
