71f4ae942683a0982919d1d890025dc1dbb31fb3
RubberDuckkie <69916215+RubberDuckkie@users.noreply.github.com>
2020-09-19 14:18:33 -0400

    Add files via upload

---
+++ PE 11/Ogunwale_PE11_No5/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogunwale_PE11_No5
+{
+    public class Vehicles
+    {
+        public virtual void LoadPassenger() { }
+
+        string[] Car = { "Compact", "SUV", "Pickup" };
+
+        string[] Train = { "PassengerTrain", "FreightTrain", "424DoubleBogey" };
+
+      
+        
+
+    
+        
+        
+
+    }
+
+    
+    }
+
+    interface IPassengerCarrirer {
+        
+            
+
+    
+    }
+
+    interface IHeavyLoadCarrier { 
+    
+    
+    }
+
