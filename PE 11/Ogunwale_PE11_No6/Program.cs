9a69565aa842a784f0aa092326a8ac7ab5ca6b7a
RubberDuckkie <69916215+RubberDuckkie@users.noreply.github.com>
2020-09-19 14:19:03 -0400

    Add files via upload

---
+++ PE 11/Ogunwale_PE11_No6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogunwale_PE11_No6
+{
+    class Traffic
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void AddPassenger(IPassengerCarrier) {
+            LoadPassenger();
+            ToString();
+
+        }
+
+    }
+}
